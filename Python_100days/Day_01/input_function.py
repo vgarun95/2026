64530a1ad8a0bc8dc2795fc92b9dc5eac896a96a
Arun <vgarun95@gmail.com>
2026-08-15 12:37:20 +0530

    Commit 2nd day notes to master

---
+++ Python_100days/Day_01/input_function.py
@@ -0,0 +1,2 @@
+input("what is your name? ")
+print("Hello" + input("what is your wife name") + "!")
---
+++ Python_100days/Day_01/variables.py
@@ -0,0 +1,14 @@
+name = "Arun"
+print(name)
+
+name = input("what is your name? ") # input function 
+print(name)
+print(len(name)) # it will print the length of the name 
+
+# Same tasks using different varaibles
+username = input("what is your wife name?")
+print(username)
+print(len(username)) 
+
+# We can also write the above task in single step as well
+print(len(input("what is your wife name? ")))
